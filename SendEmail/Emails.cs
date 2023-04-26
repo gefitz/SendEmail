ab9e43b43cc1442e2c6e2a4b8368655a7c94ad93
geovane <almeidafitz@gmail.com>
2023-04-26 16:54:58 -0300

    Adicionar arquivos de projeto.

---
+++ SendEmail/Emails.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SendEmail
+{
+    public class Email
+    {
+        public string Provedor { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+
+        public Email(string provedor, string username, string password)
+        {
+            Provedor = provedor ?? throw new ArgumentNullException(nameof(provedor));
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+            Password = password ?? throw new ArgumentNullException(nameof(password));
+        }
+
+        public void SendEmail(string emailsTo, string subject, string body)
+        {
+            var message = PrepareteMessage(emailsTo, subject, body);
+
+            SendEmailBySmtp(message);
+        }
+
+        private MailMessage PrepareteMessage(string emailsTo, string subject, string body)
+        {
+            var mail = new MailMessage();
+            mail.From = new MailAddress(Username);
+
+
+                if (ValidateEmail(emailsTo))
+                {
+                    mail.To.Add(emailsTo);
+                }
+            
+
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+
+           
+
+            return mail;
+        }
+
+        private bool ValidateEmail(string email)
+        {
+            Regex expression = new Regex(@"\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}");
+            if (expression.IsMatch(email))
+                return true;
+
+            return false;
+        }
+
+        private void SendEmailBySmtp(MailMessage message)
+        {
+            SmtpClient smtpClient = new SmtpClient("smtp.office365.com", 587);
+            smtpClient.Port = 587;
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(Username, Password);
+            smtpClient.Send(message);
+            smtpClient.Dispose();
+        }
+
+    }
+}
---
+++ SendEmail/Program.cs
@@ -0,0 +1,4 @@
+﻿using SendEmail;
+
+var gmail = new Email("smtp.office365.com", "geovanefitz@outlook.com", "92253711Ge@");
+gmail.SendEmail(emailsTo: "almeidafitz@gmail.com" ,subject:"teste",body:"segue Anexo");
